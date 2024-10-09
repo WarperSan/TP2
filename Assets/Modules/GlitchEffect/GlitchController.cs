a5be05ef491fa205ae30dfbb84f97fc7edec3d96
Hugo Jette <hugo.jette3@gmail.com>
2024-10-09 14:27:11 -0400

    Glitch Effect Animation

--- Assets/Modules/GlitchEffect/GlitchController.cs
+++ Assets/Modules/GlitchEffect/GlitchController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GlitchController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Material mat;
+    public float noiseAmount;
+    public float glitchStrength;
+    public float scanLinesStrength;
 
     // Update is called once per frame
     void Update()
     {
-        
+        mat.SetFloat("_NoiseAmount", noiseAmount);        
+        mat.SetFloat("_GlitchStrength", glitchStrength);        
+        mat.SetFloat("_ScanLinesStrength", scanLinesStrength);
     }
 }
